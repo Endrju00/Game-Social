e51513db59c6a9f612fde76e5796b7069283b38b
AM KK <andrzej.kapczynski@op.pl>
2021-07-08 23:41:42 +0200

    Joining and leaving group

--- socialmedia/groups/urls.py
+++ socialmedia/groups/urls.py
@@ -6,5 +6,7 @@ app_name = 'groups'
 urlpatterns = [
     path('', views.ListGroups.as_view(), name='all'),
     path('new/', views.CreateView.as_view(), name='create'),
-    path('posts/in/<str:slug>/, views.SingleGroup.as_view(), name='single')
+    path('posts/in/<str:slug>/', views.SingleGroup.as_view(), name='single'),
+    path('join/<str:slug>/', views.JoinGroup.as_view(), name='join'),
+    path('leave/<str:slug>/', views.LeaveGroup.as_view(), name='leave'),
 ]
